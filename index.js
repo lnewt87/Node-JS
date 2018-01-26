c5c171686a935cc4750b1d52dda6345a7c8be218
lnewt87 <lindanewt@gmail.com>
2018-01-25 21:08:40 -0500

    make it better

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.get('/', (req, res) => {
 res.send({ hi: 'there'});
 });
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT);
+//const PORT = process.env.PORT || 5000
+app.listen(https://stormy-fortress-88465.herokuapp.com);
