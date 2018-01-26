0a7ce757773ccf92b3981d7dbb66aa58a28bce45
lnewt87 <lindanewt@gmail.com>
2018-01-25 21:27:50 -0500

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+//const passport = require('passport');
+//const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+//passport.use(new GoogleStrategy());
 
 app.get('/', (req, res) => {
 res.send({ hi: 'there'});
